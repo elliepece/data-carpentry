663e807d2d3be9fd6630e9b4129e86431cfb16ab
elliepece <eperezcoronel@ucmerced.edu>
2017-08-18 11:03:30 -0700

    adding the tidyverse script

---
+++ script_for_tidyverse.R
@@ -0,0 +1,26 @@
+
+##upload data into this project
+surveys <- read.csv(data/portal_data_joined.csv)
+
+##installing tidyverse
+install.packages("tidyverse")
+
+##load tidyverse packages
+library(tidyverse)
+
+##select columns of a data frame. The first argument to this function is the data frame (surveys), and the subsequent arguments are the columns to keep.
+select(surveys, plot_id, species_id, weight)
+
+##choose rows based on a specific criteria
+filter(surveys, year == 1995)
+
+##Pipes let you take the output of one function and send it directly to the next
+surveys %>%
+  filter(weight < 5) %>%
+  select(species_id, sex, weight)
+
+##create a new object with this smaller version of the data, assigning it a new name:
+surveys_sml <- surveys %>%
+  filter(weight < 5) %>%
+  select(species_id, sex, weight)
+
